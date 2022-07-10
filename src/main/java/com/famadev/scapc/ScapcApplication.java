56be0aa76482f9d0e0b0dd5c8c969ff22d9cd952
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-10 06:35:06 -0300

    Initial commit

---
+++ src/main/java/com/famadev/scapc/ScapcApplication.java
@@ -0,0 +1,13 @@
+package com.famadev.scapc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScapcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScapcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/famadev/scapc/ScapcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famadev.scapc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScapcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
